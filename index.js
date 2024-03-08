09b4e3c9068ff69858845fdd89a8bfe0872adfe2
Thato Will Joseph <programergeek@gmail.com>
2024-03-08 08:11:57 +0200

    feat: setup secured test endpoints

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const oracledb = require('oracledb');
+const express = require("express");
+const oracledb = require("oracledb");
 
 const app = express();
 app.use(express.json());
@@ -7,3 +7,38 @@ app.use(express.json());
 const PORT = 3000;
 app.listen(PORT, () => console.log("Listening on port " + PORT));
 
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const jwtCheck = auth({
+	secret: "nZ29VCk0IE14IWfw1E7mIGOlhRPIcCer",
+	audience: "http://localhost:3000",
+	issuerBaseURL: "https://dev-yxcvikhgd4lanrwd.us.auth0.com/",
+	tokenSigningAlg: "HS256",
+});
+
+// This route doesn't need authentication
+app.get("/api/public", function (req, res) {
+	res.json({
+		message:
+			"Hello from a public endpoint! You don't need to be authenticated to see this.",
+	});
+});
+
+// This route needs authentication
+app.get("/api/private", jwtCheck, function (req, res) {
+	res.json({
+		message:
+			"Hello from a private endpoint! You need to be authenticated to see this.",
+	});
+});
+
+const checkScopes = requiredScopes("read:messages");
+
+app.get("/api/private-scoped", jwtCheck, checkScopes, function (req, res) {
+	res.json({
+		message:
+			"Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+	});
+});
