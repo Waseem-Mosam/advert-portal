d1c3d077a2a7d0b01e8ade79e1de8e33d93b2e7c
Waseem-Mosam <waseem.mosam1@gmail.com>
2024-03-26 21:54:05 +0200

    demo version

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const oracledb = require("oracledb");
 
-const config = {
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT
+
+const dbConfig = {
     user: "mhu04972",
     password: "mhu04972",
     connectString: "10.0.18.2:1521/orcl"
@@ -10,8 +12,6 @@ const config = {
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
-app.listen(PORT, () => console.log("Listening on port " + PORT));
 
 const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 
@@ -28,7 +28,7 @@ const jwtCheck = auth({
 app.get("/api/public", function (req, res) {
 	res.json({
 		message:
-			"Hello from a public endpoint! You don't need to be authenticated to see this.",
+		"Hello from a public endpoint! You don't need to be authenticated to see this.",
 	});
 });
 
@@ -36,7 +36,7 @@ app.get("/api/public", function (req, res) {
 app.get("/api/private", jwtCheck, function (req, res) {
 	res.json({
 		message:
-			"Hello from a private endpoint! You need to be authenticated to see this.",
+		"Hello from a private endpoint! You need to be authenticated to see this.",
 	});
 });
 
@@ -45,27 +45,105 @@ const checkScopes = requiredScopes("read:messages");
 app.get("/api/private-scoped", jwtCheck, checkScopes, function (req, res) {
 	res.json({
 		message:
-			"Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+		"Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
 	});
 });
 
 
-/* Staff Member endpoints */
+/**
+ * Staff endpoints
+ */
+
+// Retrieve advert by staff ID
+app.get('/staff/:staff_id/adverts', async (req, res) => {
+    let connection
+    
+    var staff_id = req.params.staff_id
+
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+        const data = await connection.execute (
+            `SELECT * 
+             FROM CSI345_ADVERT
+             WHERE SELLERID = :staff_id`, [staff_id]
+        )
+        res.json(data.rows);
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+// Retrieve all adverts
+app.get('/staff/adverts', async (req, res) => {
+    let connection;
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+        const data = await connection.execute (
+            `SELECT * 
+             FROM CSI345_ADVERT
+             WHERE NOT STATUS = 'Deleted' AND NOT STATUS = 'Pending'`
+        )
+        res.json(data.rows);
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+
+// Create Advert
+app.post('/adverts', async (req, res) => {
+    let connection;
+    
+    const advert_id = Math.floor(100000 + Math.random() * 900000);       // generate a random 6-digit number
+
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+
+        const data = await connection.execute (
+            `INSERT INTO CSI345_ADVERT (ADVERTID, TITLE, DESCRIPTION, PRICE, SELLERID) 
+             VALUES (:1, :2, :3, :4, :5)`, [advert_id, req.body.title, req.body.desc, req.body.price, req.body.seller_id], {autoCommit: true}
+        )
+        res.send(201);
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+
+// Adverts by AdvertID
+app.get('/advert/:advert_id', async (req, res) => {
+    let connection;
+    const advert_id = req.params.advert_id;
+   
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+        const data = await connection.execute (
+            `SELECT *
+             FROM CSI345_ADVERT
+             WHERE ADVERTID = :advert_id`, [advert_id]
+        )
+        res.json(data.rows);
+    } catch (err) {
+        res.send(err);
+    }
+})
 
 // Update Advert
-app.put('/adverts', async (req, res) => {
+app.put('/adverts/:advert_id', async (req, res) => {
 	let conn;
 
     
 
 	try{
-		conn = await oracledb.getConnection(config);
+		conn = await oracledb.getConnection(dbConfig);
 
-		const advert_id = req.query.id;
+		const advert_id = req.params.advert_id;
 		const { title, description, price } = req.body;
 
 		const result = await conn.execute(
-			'UPDATE CSI345_ADVERT SET TITLE = :title, DESCRIPTION = :description, PRICE = :price WHERE ADVERTID = :advert_id',
+			`UPDATE CSI345_ADVERT 
+             SET TITLE = :title, DESCRIPTION = :description, PRICE = :price 
+             WHERE ADVERTID = :advert_id`,
 			[title, description, price, advert_id], { autoCommit: true }
 		);
 
@@ -80,15 +158,36 @@ app.put('/adverts', async (req, res) => {
 	}
 })
 
+// Delete Advert
+app.put('/advert/:advert_id', async (req, res) => {
+    const advert_id = req.params.advert_id;
 
-/* Administrator endpoints */
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+        const data = await connection.execute (
+            `UPDATE CSI345_ADVERT  
+                SET STATUS = 'Deleted'
+                WHERE ADVERTID = :advert_id`, [advert_id], { autoCommit: true }
+        )
+        res.json({ "message":"Deleted" })
+    } catch (err) {
+        res.send(err);
+    }
+
+
+});
+
+
+/**
+ * Admin endpoints
+ */
 
 // Get Adverts
 app.get('/adverts', async (req, res) => {
 	let conn;
 
     try{
-        conn = await oracledb.getConnection(config);
+        conn = await oracledb.getConnection(dbConfig);
 
         const result = await conn.execute('SELECT * FROM CSI345_ADVERT');
 
@@ -109,7 +208,7 @@ app.post("/adverts/:advertId", async (req, res) => {
 	let conn;
 	let cred;
 	try {
-		conn = await oracledb.getConnection(config);
+		conn = await oracledb.getConnection(dbConfig);
 
 		const { advertId } = req.params;
 
@@ -134,7 +233,7 @@ app.get("/reports", async (req, res) => {
 	let conn;
 	const { report_type } = req.query;
 	try {
-		conn = await oracledb.getConnection(config);
+		conn = await oracledb.getConnection(dbConfig);
 		let result
 		if (report_type == "sold"){
 			result = await conn.execute("SELECT * FROM CSI345_ADVERT WHERE TAG = 'Sold'");
@@ -166,7 +265,7 @@ app.post("/login", async (req, res) => {
 	};
 
 	try {
-		conn = await oracledb.getConnection(config);
+		conn = await oracledb.getConnection(dbConfig);
 
 		const { email, password } = req.body;
 
@@ -179,7 +278,8 @@ app.post("/login", async (req, res) => {
 			try {
 				request(options, function (error, response, body) {
 					if (error) throw new Error(error);
-					res.json(body)
+                    const creds = JSON.parse(body)
+					res.json({access_token:creds.access_token})
 				});
 			} catch (err) {
 				console.error(err);
@@ -196,3 +296,6 @@ app.post("/login", async (req, res) => {
 		}
 	}
 });
+			
+const PORT = 3000;
+app.listen(PORT, () => console.log("Listening on port " + PORT));
